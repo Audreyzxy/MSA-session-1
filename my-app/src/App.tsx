27887a453a10271447ae246339487aeb7cb4e823
Audrey Zhang <audrey.zhang@mccarthyfinch.com>
2018-07-16 09:49:08 +1200

    part 1 finished

---
+++ my-app/src/App.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import './App.css';
+
+// import logo from './logo.svg';
+
+class App extends React.Component {
+  public render() {
+    return (
+      <div className="container-fluid">
+      <div className="centreText">
+        {/* React components must have a wrapper node/element */}
+        <h1>( ͡° ͜ʖ ͡°)</h1>
+      </div>
+    </div>
+    );
+  }
+}
+
+export default App;
